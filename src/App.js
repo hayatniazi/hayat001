81c286a3c285ffdd4824da803a7afb9e66cabead
hayatniazi <66178207+hayatniazi@users.noreply.github.com>
2020-06-02 23:10:39 +0100

    Update

--- src/App.js
+++ src/App.js
@@ -8,8 +8,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          ALLAH is the greatest, most merciful and most beneficient..!
         </p>
+        <p>First ReactApp by Hayat Mohammad Niazi</p>
         <a
           className="App-link"
           href="https://reactjs.org"
